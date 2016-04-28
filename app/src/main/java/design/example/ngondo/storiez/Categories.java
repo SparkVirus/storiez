22a03294ac5ec37b2048615e1b3881d95c8e339f
cngondo <devngondo@gmail.com>
2016-04-29 00:31:44 +0300

    Added tabbed layout

--- app/src/main/java/design/example/ngondo/storiez/Categories.java
+++ app/src/main/java/design/example/ngondo/storiez/Categories.java
@@ -3,11 +3,16 @@ package design.example.ngondo.storiez;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class Categories extends AppCompatActivity {
+    TabLayout mTablayout;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,14 @@ public class Categories extends AppCompatActivity {
         setContentView(R.layout.activity_categories);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //setting up viewpager from the setter
+        mViewPager = (ViewPager) findViewById(R.id.vPager);
+        setmViewPager(mViewPager);
+        //using the viewpager in the tabs
+        mTablayout = (TabLayout) findViewById(R.id.categories);
+        mTablayout.setupWithViewPager(mViewPager);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +37,15 @@ public class Categories extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void setmViewPager(ViewPager mViewPager) {
+        FragmentPageAdapter adapter = new FragmentPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fables(), "FABLES");
+        adapter.addFragment(new Epics(), "EPICS");
+        adapter.addFragment(new Legends(), "LEGENDS");
+        //point viewpager to the adapter
+        mViewPager.setAdapter(adapter);
     }
 
 }
